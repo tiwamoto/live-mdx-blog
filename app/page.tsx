cf116511737c5a47c81cb4a840473cd0a16ea5e4
takuya <takuya@gmail.com>
2024-05-12 01:34:09 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio quis rerum excepturi quo voluptates qui minima, maxime veritatis laudantium numquam? At aliquid sint, dicta vel ullam labore vitae veritatis maxime.
+      </p>
+    </main>
+  );
+}
